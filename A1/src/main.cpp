9207533fab5e1bebfefe6fa92ab2199bb5688354
Bobby Newman <bobby.newman@outlook.com>
2024-03-10 18:16:27 +0000

    Debugging.

--- A1/src/main.cpp
+++ A1/src/main.cpp
@@ -14,13 +14,19 @@ class NewShape
 public:
 	std::unique_ptr<sf::Shape> m_sprite;
 	bool m_draw = true;
-	//std::string m_textString;		Might not be necessary as we aren't changing it
+	//std::string m_textString;					Might not be necessary as we aren't changing it
 	sf::Text m_text;
 	sf::Vector2f m_velocity;
+	float m_scale = 1.f;
+	float m_colour[3] = { 0.f, 1.f, 1.f };			// imgui requires RGB values as floats from 0-1
+	char m_guiString[255];
 
 public:
-	NewShape(sf::Vector2f velocity)					// std::string text
-		: m_velocity(velocity) {}					// m_textString(text) 
+	NewShape(const std::string& shapeName, sf::Vector2f velocity)					// std::string text
+		: m_velocity(velocity)
+	{
+		strncpy_s(m_guiString, shapeName.c_str(), sizeof(m_guiString) - 1);
+	}
 
 	void move()
 	{
@@ -29,13 +35,13 @@ public:
 	}
 };
 
-void readConfigFile(std::ifstream& file, std::vector<std::unique_ptr<NewShape>>& shapes, sf::Vector2i& windowDimensions)
+void readConfigFile(std::ifstream& file, std::vector<std::shared_ptr<NewShape>>& shapes, sf::Vector2i& windowDimensions)
 {
 	// For all shapes' text
 	sf::Font font;
 	std::string fontPath;
-	int textSize;
-	int textColour[3];
+	int textSize = 0;
+	int textColour[3] = { 0.f, 0.f, 0.f };
 
 	// For reading config file
 	std::string line;
@@ -72,7 +78,7 @@ void readConfigFile(std::ifstream& file, std::vector<std::unique_ptr<NewShape>>&
 				lineStream >> shapeText >> position.x >> position.y >> velocity.x >> velocity.y >> r >> g >> b;
 
 				// Instantiate NewShape
-				std::unique_ptr<NewShape> shape = std::make_unique<NewShape>(velocity);
+				std::shared_ptr<NewShape> shape = std::make_shared<NewShape>(shapeText, velocity);
 
 				if (firstElement == "Rectangle")
 				{
@@ -93,8 +99,8 @@ void readConfigFile(std::ifstream& file, std::vector<std::unique_ptr<NewShape>>&
 				shape->m_text = sf::Text(shapeText, font, textSize);
 				shape->m_text.setFillColor(sf::Color(textColour[0], textColour[1], textColour[2]));
 
-				// Add shape to vector of shape pointers
-				shapes.push_back(std::move(shape));
+				// Add shape to vector of shape pointers; std::move() required because shape is a std::unique_ptr
+				shapes.push_back(shape);
 			}
 		}
 	}
@@ -111,7 +117,7 @@ int main(int argc, char* argv[])
 	}
 
 	// Vector of shapes
-	std::vector<std::unique_ptr<NewShape>> shapes;
+	std::vector<std::shared_ptr<NewShape>> shapes;
 
 	// For window; defaults to these unless changed by file
 	sf::Vector2i windowDimensions(1920, 1080);
@@ -129,19 +135,6 @@ int main(int argc, char* argv[])
 	ImGui::GetStyle().ScaleAllSizes(3.5f);		// Scales imgui GUI
 	ImGui::GetIO().FontGlobalScale = 1.3f;		// Scale imgui text size
 
-	/*				I think these should be attributes of each shape?
-	
-	float c[3] = { 0.0f, 1.0f, 1.0f };	// imgui requires RGB values as floats from 0-1
-
-	float scale = 50.0f;
-	bool drawShape = true;
-	bool drawText = true;
-
-	// Array for imgui text
-	char displayString[255] = "Sample Text";
-
-	*/
-
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -151,68 +144,79 @@ int main(int argc, char* argv[])
 			ImGui::SFML::ProcessEvent(window, event);
 
 			if (event.type == sf::Event::Closed)
-			{
 				window.close();
-			}
 		}
 
 		// Updates imgui for this frame with the time the last frame took; .restart() returns the sf::Time object and restarts the clock
 		ImGui::SFML::Update(window, deltaClock.restart());
 
-		// Draw UI for each shape
-		/* for std::unique_ptr<sf::NewShape> : shapes
+		//ImGui::ShowDemoWindow();
+
+		static int dropdownIndex = 0;
+
+		// GUI dropdown box
+		if (ImGui::BeginCombo("##dropdown", shapes[dropdownIndex]->m_text.getString().toAnsiString().c_str()))
 		{
-			// ImGui shit from below
+			for (int i = 0; i < shapes.size(); i++)
+			{
+				const bool isSelected = (dropdownIndex == i);
+				if (ImGui::Selectable(shapes[i]->m_text.getString().toAnsiString().c_str()), isSelected)
+					dropdownIndex = i;
+
+				if (isSelected)
+					ImGui::SetItemDefaultFocus();
+			}
+			ImGui::EndCombo();
 		}
-		*/
+
+		std::shared_ptr<NewShape> currentShape = shapes[dropdownIndex];
 
 		// Draw the UI
 		ImGui::Begin("Shape Properties");
-		ImGui::Checkbox("Draw Shape", &drawShape);
-		ImGui::SliderFloat(&shape->m_velocity.x, 0.0f, 100.f);					// Does it need text?
+		ImGui::Checkbox("Draw Shape", &currentShape->m_draw);
+		ImGui::SliderFloat("##XVelocity", &currentShape->m_velocity.x, 0.0f, 100.f);					// Does it need text?
 		ImGui::SameLine();
-		ImGui::SliderFloat("Velocity", &shape->m_velocity.y, 0.0f, 100.f);
-		ImGui::SliderFloat("Scale", &scale, 0.0f, 300.0f);
-		ImGui::ColorEdit3("Color", c);
-		ImGui::InputText("Name", displayString, 255);							// Should directly change text, as below
-		shape->m_text.setString(displayString);									// THIS IS CORRECT
-		ImGui::SameLine();
+		ImGui::SliderFloat("Velocity", &currentShape->m_velocity.y, 0.0f, 100.f);
+		ImGui::SliderFloat("Scale", &currentShape->m_scale, 0.0f, 300.0f);
+		ImGui::ColorEdit3("Color", currentShape->m_colour);
+		ImGui::InputText("Name", currentShape->m_guiString, 255);									// Should directly change text, as below
 		ImGui::End();
+
+		currentShape->m_text.setString(currentShape->m_guiString);
+		currentShape->m_sprite->setFillColor(sf::Color(currentShape->m_colour[0] * 255, currentShape->m_colour[1] * 255, currentShape->m_colour[2] * 255));		// converts from a value 0-1 to 0-255
+		currentShape->m_sprite->setScale(sf::Vector2f(currentShape->m_scale, currentShape->m_scale));			// was .setRadius()
 
 		// Update logic
 		for (auto& shape : shapes)
 		{
-			const sf::FloatRect& shapeBounds = shape->m_sprite->getGlobalBounds();\
-			const sf::FloatRect& textBounds = shape->m_text->getLocalBounds();
-			if (shapeBounds.left < 0 || shapeBounds.left + shapeBounds.width > wWidth)
-				shape->velocity.x *= -1.0f;
+			const sf::FloatRect& shapeBounds = shape->m_sprite->getGlobalBounds();
+			const sf::FloatRect& textBounds = shape->m_text.getLocalBounds();
 
-			if (shapeBounds.top < 0 || shapeBounds.top + shapeBounds.height > wHeight)
-				shape->velocity.y *= -1.0f;
+			// If bounds exceed window edge, reverse corresponding velocity
+			if (shapeBounds.left < 0 || shapeBounds.left + shapeBounds.width > windowDimensions.x)
+				shape->m_velocity.x *= -1.0f;
+
+			if (shapeBounds.top < 0 || shapeBounds.top + shapeBounds.height > windowDimensions.y)
+				shape->m_velocity.y *= -1.0f;
 
 			shape->move();
 
 			// Sets text to center of shape
 			shape->m_text.setPosition(shapeBounds.left + shapeBounds.width / 2.f - textBounds.width / 2.f, shapeBounds.top + shapeBounds.height / 2.f - textBounds.height / 2.f);
-
-			shape->m_sprite->setFillColour(sf::Color(c[0] * 255, c[1] * 255, c[2] * 255));		// converts from a value 0-1 to 0-255
-			shape->m_sprite->setScale(scale);			// INSTEAD OF .setRadius() -> .scale()
 		}
 
 		// Clear window
 		window.clear();
 
 		// Draw each shape to window
-		/*
 		for (auto& shape : shapes)
 		{
 			if (shape->m_draw)
 			{
-				window.draw(shape->m_sprite);
+				window.draw(*(shape->m_sprite));
 				window.draw(shape->m_text);
 			}
 		}
-		*/
 
 		ImGui::SFML::Render(window);			// Render the imgui GUI to the back buffer window
 		window.display();						// Switches the front and back buffers
