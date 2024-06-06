43db875bcb4adf1a0b8c3b74ecba39835417709e
BobbyPC <bobby.newman@outlook.com>
2024-06-06 15:34:05 +0100

    Commented out ImGui demo window to fix assignment 2.

--- A2/src/Game.cpp
+++ A2/src/Game.cpp
@@ -87,9 +87,9 @@ void Game::init(const std::string& configFile)
     file.close();
 
     // This isn' allowed?
-    m_window = sf::RenderWindow(sf::VideoMode(1920, 1080), "Assignment 2");
-    // m_window.create(sf::VideoMode(static_cast<unsigned int>(m_resolution.x), static_cast<unsigned int>(m_resolution.y)), "Assignment 2");
-    //m_window.create(sf::VideoMode(1920, 1080), "Assignment 2");
+    //m_window = sf::RenderWindow(sf::VideoMode(1920, 1080), "Assignment 2");
+    //m_window.create(sf::VideoMode(static_cast<unsigned int>(m_resolution.x), static_cast<unsigned int>(m_resolution.y)), "Assignment 2");
+    m_window.create(sf::VideoMode(1920, 1080), "Assignment 2");
     m_window.setFramerateLimit(60);
 
     // Why is the window size 1440, 847?
@@ -447,7 +447,7 @@ void Game::sGUI()
 {   
     ImGui::SFML::Update(m_window, m_deltaClock.restart());
 
-    ImGui::ShowDemoWindow();
+    // ImGui::ShowDemoWindow();
 
     ImGui::Begin("Shape Shooter");
 
